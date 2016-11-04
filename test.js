3c7331a06c235e50899a197e8babe7630a3b2103
Titus Wormer <tituswormer@gmail.com>
2016-11-04 17:26:53 +0100

    Remove file-overview inline docs

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
-/**
- * @author Titus Wormer
- * @copyright 2015 Titus Wormer
- * @license MIT
- * @module ccount
- * @fileoverview Test suite for `ccount`.
- */
-
 'use strict';
 
 /* Dependencies. */
